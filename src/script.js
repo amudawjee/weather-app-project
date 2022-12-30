eb11449c23d8f25f1f23fec85af890bbdbe06754
amudawjee <ammarahdawjee@gmail.com>
2022-12-30 17:07:55 +0000

    Added unit conversion functionality

--- src/script.js
+++ src/script.js
@@ -24,20 +24,21 @@ function findDateTime() {
 
 function showWeather(response) {
   let city = response.data.name;
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = city;
-  let temperature = Math.round(response.data.main.temp);
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = `${temperature}°C`;
-  let tempMin = Math.round(response.data.main.temp_min);
-  let tempMax = Math.round(response.data.main.temp_max);
-  let h3 = document.querySelector("h3");
-  h3.innerHTML = `${tempMax}°C/${tempMin}°C`;
-  let h5 = document.querySelector("h5");
-  h5.innerHTML = response.data.weather[0].description;
-  let h6 = document.querySelector("h6");
-  h6.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  let location = document.querySelector("h1");
+  let currentTemp = document.querySelector("h2");
+  let tempRange = document.querySelector("h3");
+  let comment = document.querySelector("h5");
+  let humidity = document.querySelector("h6");
   let icon = document.querySelector("img");
+
+  location.innerHTML = city;
+  temperature = Math.round(response.data.main.temp);
+  currentTemp.innerHTML = `${temperature}°C`;
+  tempMin = Math.round(response.data.main.temp_min);
+  tempMax = Math.round(response.data.main.temp_max);
+  tempRange.innerHTML = `${tempMax}°C/${tempMin}°C`;
+  comment.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   icon.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -53,8 +54,6 @@ function submitSearch(event) {
 }
 
 function citySearch(city) {
-  let unit = "metric";
-  let apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
   axios.get(apiUrl).then(showWeather);
 }
@@ -62,8 +61,6 @@ function citySearch(city) {
 function showPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  let unit = "metric";
-  let apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
   let geoUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
   axios.get(geoUrl).then(showWeather);
 }
@@ -73,11 +70,53 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
+function changeCelcius(event) {
+  document.getElementById("convert-celcius").classList.remove("active");
+  document.getElementById("convert-celcius").classList.add("inactive");
+  document.getElementById("convert-farenheit").classList.remove("inactive");
+  document.getElementById("convert-farenheit").classList.add("active");
+
+  farenheitTemp = Math.round((temperature * 9) / 5 + 32);
+  farenheitMax = Math.round((tempMax * 9) / 5 + 32);
+  farenheitMin = Math.round((tempMin * 9) / 5 + 32);
+
+  let currentTemp = document.querySelector("h2");
+  let tempRange = document.querySelector("h3");
+
+  currentTemp.innerHTML = `${farenheitTemp}°F`;
+  tempRange.innerHTML = `${farenheitMax}°F/${farenheitMin}°F`;
+}
+
+function changeFarenheit(event) {
+  document.getElementById("convert-celcius").classList.add("active");
+  document.getElementById("convert-celcius").classList.remove("inactive");
+  document.getElementById("convert-farenheit").classList.add("inactive");
+  document.getElementById("convert-farenheit").classList.remove("active");
+
+  let currentTemp = document.querySelector("h2");
+  let tempRange = document.querySelector("h3");
+
+  currentTemp.innerHTML = `${temperature}°C`;
+  tempRange.innerHTML = `${tempMax}°C/${tempMin}°C`;
+}
+
+let unit = "metric";
+let apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
+let temperature = "";
+let tempMin = "";
+let tempMax = "";
+
 let changeLocation = document.querySelector("#search-button");
 changeLocation.addEventListener("click", submitSearch);
 
 let currentLocation = document.querySelector("#current-location");
 currentLocation.addEventListener("click", getCurrentLocation);
+
+let convertCelcius = document.querySelector("#convert-celcius");
+convertCelcius.addEventListener("click", changeCelcius);
+
+let convertFarenheit = document.querySelector("#convert-farenheit");
+convertFarenheit.addEventListener("click", changeFarenheit);
 
 findDateTime();
 citySearch("London");
